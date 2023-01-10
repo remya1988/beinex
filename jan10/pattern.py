b321c4c3506bc218dbfd69075e6149d6d99fb5d1
remya1988 <106645101+remya1988@users.noreply.github.com>
2023-01-10 17:50:01 +0530

    Update pattern.py

--- jan10/pattern.py
+++ jan10/pattern.py
@@ -1,1 +1,37 @@
 
+lmt=int(input("Enter the limit : "))
+
+# First pattern
+print("***** First pattern *****")
+k = lmt - 1
+for i in range(0, lmt):
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("\r")
+
+# Second pattern
+print("***** Second pattern *****")
+for i in range(0, lmt):
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("\r")
+
+# Third pattern
+print("***** Third pattern *****")
+for i in range(0, lmt):
+    for j in range(0, i + 1):
+        print(i, end=" ")
+    print("\r")
+
+# Fourth pattern
+print("***** Fourth pattern *****")
+k=0
+for i in range(0, lmt):
+    for j in range(0, i + 1):
+        # printing stars
+        print(k, end=" ")
+        k+=1
+    print("\r")
